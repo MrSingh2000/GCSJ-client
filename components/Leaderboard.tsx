84704bef9b1679096083f15f2c483611cc394067
Anshuman Singh <www.anshu2000@gmail.com>
2023-10-05 22:16:25 +0530

    v1.1

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -15,9 +15,9 @@ function Leaderboard({ data, sortData }: Props) {
   return (
     <div className='h-full min-h-screen w-full'>
 
-      <div className="absolute right-0 mr-5 mt-5 inline-block text-left z-10">
+      <div className="absolute right-0 mr-5 inline-block text-left z-10">
         <div>
-          <button onClick={() => { setModal((prev) => !prev) }} type="button" className=" border border-gray-300 bg-white dark:bg-gray-800 shadow-sm flex items-center justify-center w-full rounded-md  px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-50 hover:bg-gray-50 dark:hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500" id="options-menu">
+          <button onClick={() => { setModal((prev) => !prev) }} type="button" className="border border-gray-300 bg-white dark:bg-gray-800 shadow-sm flex items-center justify-center w-full rounded-md  px-4 py-2 text-sm font-medium text-gray-700 dark:text-gray-50 hover:bg-gray-50 dark:hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500" id="options-menu">
             Sort
             <svg width="20" height="20" fill="currentColor" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
               <path d="M1408 704q0 26-19 45l-448 448q-19 19-45 19t-45-19l-448-448q-19-19-19-45t19-45 45-19h896q26 0 45 19t19 45z">
@@ -27,27 +27,34 @@ function Leaderboard({ data, sortData }: Props) {
         </div>
         {modal && <div className="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg dark:bg-gray-800 ring-1 ring-black ring-opacity-5">
           <div className="py-1 " role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-            <a href="#" onClick={() => { sortData("courses"); setModal((prev) => !prev) }} className="block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
+            <button onClick={() => { sortData("courses"); setModal((prev) => !prev) }} className="w-full text-left block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
               <span className="flex flex-col">
                 <span>
                   Courses
                 </span>
               </span>
-            </a>
-            <a href="#" onClick={() => { sortData("badges"); setModal((prev) => !prev) }} className="block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
+            </button>
+            <button onClick={() => { sortData("badges"); setModal((prev) => !prev) }} className="w-full text-left block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
               <span className="flex flex-col">
                 <span>
                   Skill Badges
                 </span>
               </span>
-            </a>
-            <a href="#" onClick={() => { sortData("genai"); setModal((prev) => !prev) }} className="block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
+            </button>
+            <button onClick={() => { sortData("genai"); setModal((prev) => !prev) }} className="w-full text-left block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
               <span className="flex flex-col">
                 <span>
                   GenAi Game
                 </span>
               </span>
-            </a>
+            </button>
+            <button className="w-full text-left block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600" role="menuitem">
+              <span className="flex flex-col">
+                <span>
+                  LeaderBoard (coming soon)
+                </span>
+              </span>
+            </button>
           </div>
         </div>}
       </div>
