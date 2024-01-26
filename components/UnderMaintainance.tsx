9c667aaf866e496d918d5e1f3fb90240997b56d5
Anshuman Singh <www.anshu2000@gmail.com>
2024-01-26 16:03:56 +0530

    bugs fixes v2

--- components/UnderMaintainance.tsx
+++ components/UnderMaintainance.tsx
@@ -6,7 +6,7 @@ function UnderMaintainance({ }: Props) {
     return (
         <>
             <div class="maintainance font-mono">
-                <h1>We'll be back </h1>
+                <h1>We&apos;ll be back </h1>
                 <hr />
                 <p>Prizes ready! 🏆🎉 Stay tuned for more GDSC DCRUST fun. See you next time! 🚀</p>
             </div>
