a7c3b74929be6b505a55da42fd6d30a76e52ffd8
Anshuman Singh <www.anshu2000@gmail.com>
2024-01-26 16:09:37 +0530

    bugs v3

--- components/UnderMaintainance.tsx
+++ components/UnderMaintainance.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 type Props = {}
 
-function UnderMaintainance({ }: Props) {
+function UnderMaintainance({ }: Props): React.FC<Props> {
     return (
         <>
-            <div class="maintainance font-mono">
+            <div className="maintainance font-mono">
                 <h1>We&apos;ll be back </h1>
                 <hr />
                 <p>Prizes ready! 🏆🎉 Stay tuned for more GDSC DCRUST fun. See you next time! 🚀</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     screens: {
-      'mob': {'min': '300px', 'max': '900px'},
+      mob: { min: "300px", max: "900px" },
     },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+  compilerOptions: {
+    // other options...
+    jsx: "react",
+  },
+};
+export default config;
